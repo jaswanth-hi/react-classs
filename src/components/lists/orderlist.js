48ce0d79abb170b177ad7380750a73d0d0501365
jaswanth reddy <jaswanth-hi>
2024-07-11 16:57:10 +0530

    july09classpracties

---
+++ src/components/lists/orderlist.js
@@ -0,0 +1,9 @@
+import ListItems from "./list_items";
+
+const List=()=>{
+    return(<ol>
+        <ListItems></ListItems>
+    </ol>
+    );
+}
+export default List;
---
+++ src/components/profile/profileComm.js
@@ -0,0 +1,14 @@
+
+
+import Image from "../image/custoimage";
+import CustomHeading, { CustomHeading1 } from "../headings/headings";
+
+const Profile=()=>{
+    return(<div>
+        <Image></Image>
+        <CustomHeading></CustomHeading>
+        <CustomHeading1></CustomHeading1>
+
+    </div>)
+}
+export default Profile;
